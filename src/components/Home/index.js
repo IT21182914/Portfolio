0dd74f8dfc11af6737ab01010750f8d04ba8c0e6
IT21182914 <it21182914@my.sliit.lk>
2023-07-07 05:41:43 +0530

    New Animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import LogoTitle from '../../assets/images/download11-removebg-preview.png'
-import Logo from './Logo'
-import './index.scss'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import LogoTitle from '../../assets/images/download11-removebg-preview.png';
+import Logo from './Logo';
+import './index.scss';
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate');
 
-  const nameArray = ['i', 'l', 'a', 'n', '', 'S', 'h', 'a', 'n', 'u', 'k', 'a']
+  const nameArray = ['i', 'l', 'a', 'n', '', 'S', 'h', 'a', 'n', 'u', 'k', 'a'];
   const jobArray = [
     'W',
     'e',
@@ -25,13 +25,14 @@ const Home = () => {
     'e',
     'r',
     '.',
-  ]
+  ];
 
   useEffect(() => {
-    return () => setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-  }, [])
+    return () =>
+      setTimeout(() => {
+        setLetterClass('text-animate-hover');
+      }, 4000);
+  }, []);
 
   return (
     <>
@@ -65,11 +66,18 @@ const Home = () => {
           </Link>
         </div>
         <Logo />
+        <div className="right-image-container">
+          <img
+            className="right-image"
+            src="https://camo.githubusercontent.com/c1dcb74cc1c1835b1d716f5051499a2814c683c806b15f04b0eba492863703e9/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f3733303730332f73637265656e73686f74732f363538313234332f6176656e746f2e676966"
+            alt="Animated Gif"
+          />
+        </div>
       </div>
 
       <Loader type="pacman" />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
