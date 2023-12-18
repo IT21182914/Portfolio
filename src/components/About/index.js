6be3cd8f4b0f1d64027f1687d3f3e1eb3928766e
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-18 18:31:57 +0530

    New

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react'
+// About.jsx
+
+import React, { useEffect, useState } from 'react';
 import DilanShanukaPDF from '../../assets/images/DilanShanuka.pdf';
-
-
-
 import {
   faFigma,
   faCss3,
@@ -13,20 +12,21 @@ import {
   faJava,
   faNode,
   faPhp
-} from '@fortawesome/free-brands-svg-icons'
-import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './index.scss'
+} from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './index.scss';
 
 const About = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate');
 
   useEffect(() => {
-    return () => setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 3000)
-  }, [])
+    return () =>
+      setTimeout(() => {
+        setLetterClass('text-animate-hover');
+      }, 3000);
+  }, []);
 
   const handleDownloadCV = () => {
     fetch(DilanShanukaPDF)
@@ -43,7 +43,7 @@ const About = () => {
         console.error('Error downloading CV:', error);
       });
   };
-  
+
   return (
     <>
       <div className="container about-page">
@@ -56,35 +56,28 @@ const About = () => {
             />
           </h1>
           <p>
-          I am a 3rd-year undergraduate at SLIIT, 
-          seeking a software engineering internship. 
-          With a strong passion for software engineering, 
-          I am excited to apply my knowledge and skills in a professional 
-          environment. I am particularly interested in working on 
-          projects that involve cutting-edge technologies
-           and offer opportunities for growth and learning.
+            I am a 3rd-year undergraduate at SLIIT, seeking a software engineering internship. With a strong passion for
+            software engineering, I am excited to apply my knowledge and skills in a professional environment. I am
+            particularly interested in working on projects that involve cutting-edge technologies and offer opportunities
+            for growth and learning.
           </p>
           <p align="LEFT">
-          I have hands-on experience in the MERN stack and
-           Kotlin with Android Studio through university projects.
-            This has given me a strong foundation in both front-end and
-             back-end development,
-           enabling me to create dynamic web applications and mobile apps.
+            I have hands-on experience in the MERN stack and Kotlin with Android Studio through university projects.
+            This has given me a strong foundation in both front-end and back-end development, enabling me to create
+            dynamic web applications and mobile apps.
           </p>
           <p>
-          In addition to my technical skills, 
-          I'm a dedicated cricketer and skilled guitarist & trumpet player.
-           These interests contribute to my problem-solving abilities and 
-           attention to detail, making me a well-rounded developer. 
-           I'm ready to bring my diverse background
-           and enthusiasm for technology to an established IT company.
+            In addition to my technical skills, I'm a dedicated cricketer and skilled guitarist & trumpet player. These
+            interests contribute to my problem-solving abilities and attention to detail, making me a well-rounded
+            developer. I'm ready to bring my diverse background and enthusiasm for technology to an established IT company.
           </p>
-          <button className='button' onClick={handleDownloadCV}>Download CV</button>
+          <button className="button" onClick={handleDownloadCV}>
+            Download CV
+          </button>
         </div>
 
         <div className="stage-cube-cont">
           <div className="cubespinner">
-           
             <div className="face2">
               <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
@@ -114,10 +107,11 @@ const About = () => {
             </div>
           </div>
         </div>
+        
       </div>
       <Loader type="pacman" />
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
