8281b5eba0e80c4dc7af1bc2d1c823cb5e4397c0
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-13 11:13:00 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Layout from './components/Layout';
 import Home from './components/Home';
@@ -7,27 +7,52 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 
-
 function App() {
   return (
-    
-  <>
-    <Routes>
-    <Route path="/" element={<div><Home /><Layout /></div>} />
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Home />
+                <Layout />
+              </div>
+            }
+          />
 
-      <Route path="/about" element={<div><About /><Layout /></div>} /> 
-      <Route path="/contact" element={<div><Contact /><Layout /></div>} />
-      <Route path="/portfolio" element={<div><Portfolio /><Layout /></div>} />
-     
-     
-    
-    </Routes>
-  
-    
-    
-    
-  </>
-  )
+          <Route
+            path="/about"
+            element={
+              <div>
+                <About />
+                <Layout />
+              </div>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <div>
+                <Contact />
+                <Layout />
+              </div>
+            }
+          />
+          <Route
+            path="/portfolio"
+            element={
+              <div>
+                <Portfolio />
+                <Layout />
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
