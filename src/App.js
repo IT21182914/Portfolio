de0e5dd385906abf90f7070e8063ee485ffa3f34
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-13 11:30:43 +0530

    Worked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Layout from './components/Layout';
 import Home from './components/Home';
@@ -10,50 +9,45 @@ import Portfolio from './components/Portfolio';
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <Home />
-                <Layout />
-              </div>
-            }
-          />{' '}
-          <Route />
-          <Route
-            path="/about"
-            element={
-              <div>
-                <About />
-                <Layout />
-              </div>
-            }
-          />
-          <Route />
-          <Route
-            path="/contact"
-            element={
-              <div>
-                <Contact />
-                <Layout />
-              </div>
-            }
-          />{' '}
-          <Route />
-          <Route
-            path="/portfolio"
-            element={
-              <div>
-                <Portfolio />
-                <Layout />
-              </div>
-            }
-          />{' '}
-          <Route />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Home />
+              <Layout />
+            </div>
+          }
+        />{' '}
+        <Route
+          path="/about"
+          element={
+            <div>
+              <About />
+              <Layout />
+            </div>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <div>
+              <Contact />
+              <Layout />
+            </div>
+          }
+        />{' '}
+        <Route
+          path="/portfolio"
+          element={
+            <div>
+              <Portfolio />
+              <Layout />
+            </div>
+          }
+        />{' '}
+        <Route />
+      </Routes>
     </>
   );
 }
